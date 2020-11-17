ed770bb1f49b7b0db7f11988bcae689447e68f7b
Vitaly Vertyakov <ezhevit@gmail.com>
2020-11-17 23:10:51 +0300

    edited comments in code

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,14 @@
 # Реализуем Page Object, который будет связан с главной страницей интернет-магазина
 
-# Импорт базового класса BasePage
-from .base_page import BasePage
+from .base_page import BasePage # Импорт базового класса BasePage
 from selenium.webdriver.common.by import By
 
-# Класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка
-class MainPage(BasePage):
+class MainPage(BasePage):   # Класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка
     def go_to_login_page(self):
+        # В аргументы больше не надо передавать экземпляр браузера, мы его передаем и сохраняем на этапе создания Page Object.
+        # Вместо него нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класса
+
         # Так как браузер у нас хранится как аргумент класса BasePage,
         # обращаться к нему нужно соответствующим образом с помощью self
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
-        
